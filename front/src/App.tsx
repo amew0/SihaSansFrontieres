353419335d8535108a27225516ec8d71ca1e895a
amew0 <aminekidane4@gmail.com>
2024-04-27 11:13:49 +0400

    git initi

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <div className="bg-red-500 flex flex-col">
+    <div>FILE UPLOAD COMPONENT</div>
+    <div>OUTPUT IMAGE AND SCORE</div>
+    <div>SOME MORE REPORTS</div>
+    </div>
+  )
+}
+
+export default App
