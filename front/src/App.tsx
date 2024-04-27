596722a8067fe667c91dc1d3b7216257e0a83a5c
amew0 <aminekidane4@gmail.com>
2024-04-27 11:32:56 +0400

    add dummy components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [image, setImage] = useState(null);
+  const [results, setResults] = useState({ stats: null, chart: null });
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files![0];
+    if (file) {
+      console.log(file);
+    }
+  };
+
   return (
-    <div className="bg-red-500 flex flex-col">
-    <div>FILE UPLOAD COMPONENT</div>
-    <div>OUTPUT IMAGE AND SCORE</div>
-    <div>SOME MORE REPORTS</div>
+    <div className="flex flex-col w-screen h-screen items-center justify-center">
+      <div className="input-page">
+        <input type="file" onChange={handleImageUpload} />
+      </div>
+      {image && (
+        <div className="result-page">
+          <div className="image-result">
+            <img src={image} alt="Uploaded" />
+          </div>
+          <div className="stats-result">
+            <p>{results.stats}</p>
+          </div>
+          <div className="chart-result">
+            <p>{results.chart}</p>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
